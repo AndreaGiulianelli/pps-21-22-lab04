ec99041b765df4dab821de4043f8e51a1ab6e521
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-23 16:21:48 +0100

    red green es3

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -1,35 +1,39 @@
 package u04lab.polyglot.a01b
 
-import u04lab.polyglot.Pair
 import u04lab.polyglot.*
 import u04lab.code.Option.*
 import u04lab.code.List
+import u04lab.code.Stream
 import u04lab.code.List.*
-
-/*
-   * Scopo di questo esercizio è realizzare una GUI con l'aspetto mostrato nell'immagine fig.png, fornita,
-   * che realizza una mini-versione di Campo Minato. All'avvio si prepara un campo di gara quadrato
-   * (dimensione indicata dal primo parametro del costruttore), e si collocano in modo random le mine (in numero
-   * indicato nel secondo parametro) facendo attenzioni che siano in posizioni distinte
-   * (si stampi su console la loro posizione, a fini di debug).
-   * Alla pressione di un pulsante, questo si disabilita: se contiene una mina si esca indicando la sconfitta,
-   * altrimenti si mostri sul pulsante quante mine sono presenti in un vicino della cella clickata
-   * (in orizzontale, verticale o diagonale).
-   * Colpite tutte le celle che non hanno delle mine, si avrà vinto: si produca una stampa a console e si esca.\
-
-  Step:
-    - colloca le mine all'inizio in modo random in posizioni DISTINTE, stampando le posizioni
-    - disabilitare il pulsante quando premuto
-    - controllo se il pulsante contiene mina -> in caso esci sconfitto
-    - mostra in caso corretto il numero di vicini (orr, vert, diag)
-    - check rimanenti solo mine -> vittoria
-*/
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
-  private val minesList: List[Pair[Int, Int]] = Nil()
+  private var minesList: List[Pair[Int, Int]] = Nil()
+  private var selected: List[Pair[Int, Int]] = Nil()
+  private val rnd = Random()
+
+  placeMines(mines)
+  println(minesList)
 
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    OptionToOptional(None()) // Option => Optional converter
+    if !List.contains(minesList, Pair(x, y)) then
+      {selected = Cons(Pair(x, y), selected); OptionToOptional(Some(neighbours(x, y)))}
+    else OptionToOptional(None())
 
-  def won = false
+  def won = mines + List.length(selected) == size * size
+  // Getter used in tests
+  def getMines: List[Pair[Int, Int]] = minesList
+  def getSelected: List[Pair[Int, Int]] = selected
+
+  private def neighbours(x: Int, y: Int): Int =
+   List.length(
+     List.filter(
+       List.flatMap(
+        Stream.toList(Stream.take(Stream.iterate(x - 1)(_ + 1))(3)))
+        (xx => List.map(Stream.toList(Stream.take(Stream.iterate(y - 1)(_ + 1))(3)))(Pair(xx, _))))
+     (List.contains(minesList, _)))
+
+  private def placeMines(nMines: Int): Unit = if nMines > 0 then Pair(rnd.nextInt(size), rnd.nextInt(size)) match
+    case p if !List.contains(minesList, p) => {minesList = Cons(p, minesList); placeMines(nMines - 1)}
+    case _ => placeMines(nMines)
--- src/test/scala/u04lab/polyglot/TestLogists.scala
+++ src/test/scala/u04lab/polyglot/TestLogists.scala
@@ -1,5 +1,44 @@
 package scala.u04lab.polyglot
 
+import org.junit.Test
+import org.junit.Assert.{assertEquals, assertNotEquals, assertNotNull, assertTrue}
+import u04lab.polyglot.a01b.LogicsImpl
+import u04lab.code.List
+import u04lab.code.List.*
+import u04lab.polyglot.Pair
+import java.util.Optional
+import u04lab.code.Option
+
+import scala.util.Random
+
 class TestLogists {
-  
+  private val size = 4
+  private val mines = 2
+  private val logics = LogicsImpl(size, mines)
+  private val rnd = Random()
+
+  @Test
+  def testInit: Unit =
+    assertTrue(List.length(logics.getMines) == mines)
+
+  @Test
+  def testHit(): Unit =
+    val point = selectPointNotAMine()
+    assertNotEquals(logics.hit(point.getX, point.getY), Optional.empty())
+
+  @Test
+  def testHitMine(): Unit =
+    val point = Option.orElse(List.find(logics.getMines)(x => true), Pair(0, 0))
+    assertEquals(logics.hit(point.getX, point.getY), Optional.empty())
+
+  @Test
+  def testWon(): Unit =
+    for i <- 1 to size * size - mines do
+      val point = selectPointNotAMine()
+      logics.hit(point.getX, point.getY)
+    assertTrue(logics.won)
+
+  private def selectPointNotAMine(): Pair[Int, Int] = (rnd.nextInt(size), rnd.nextInt(size)) match
+    case (x, y) if List.contains(logics.getMines, Pair(x, y)) => selectPointNotAMine()
+    case (x, y) => Pair(x, y)
 }
